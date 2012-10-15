f36bb42ac73afb802420e18ed0f398967e39fdb5
Marsel Zaripov <marszaripov@gmail.com>
2012-10-15 17:47:16 +0400

    add example of loop using

--- controllers/first.py
+++ controllers/first.py
@@ -3,8 +3,10 @@ from lib.template import Template, Context
 
 def index(request):
     template = Template('index.html')
+    a = [1, 2, 3, 4, 5]
     context = Context({'title': 'Главная страница', 
-                       'welcome_text': 'Добро пожаловать!'})
+                       'welcome_text': 'Добро пожаловать!', 
+                       'array': a})
     result = template.render(context)
     return result
 
