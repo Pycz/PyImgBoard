7318a827b83d7b8e184a0b236379406bf935bf0c
Marsel Zaripov <marszaripov@gmail.com>
2012-10-19 18:03:19 +0400

    add blocks and ability to inherit

--- lib/template.py
+++ lib/template.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import os.path
 import sys
 import inspect
 from lib import utils
@@ -9,11 +10,17 @@ import settings
 class Template:
     def __init__(self, template_name):
         self.template_name = template_name
-        self.tags = {'for': self._loop, 'if': self._condition}
-        self.endtags = ['endfor', 'endif']
-        self.pairs = {'for': 'endfor', 'if': 'endif'}
+        self.tags = {'for': self._loop, 'if': self._condition, 'block': self._block}
+
+        self.endtags = []
+        self.pairs = {}
+        for key in self.tags.keys():
+            self.endtags.append('end' + key)
+            self.pairs[key] = self.endtags[-1]
+
         self.tags_stack = []
         self.template = None
+        self.blocks = None
 
     def render(self, context):
         self.context = context
@@ -21,7 +28,7 @@ class Template:
         root_dir = utils.get_root_dir()
         for dir in settings.TEMPLATE_DIRS:
             try:
-                self.template = open(root_dir + dir + self.template_name)
+                self.template = open(root_dir +  dir + self.template_name)
                 break
             except IOError:
                 #TODO tell to client about error
@@ -38,6 +45,8 @@ class Template:
         return output
 
     def _text(self):
+        if self._is_extends():
+            return self._extends()
         text = ''
         buf = None
         state = 0
@@ -68,7 +77,7 @@ class Template:
 
         return text
 
-    def _variable(self):
+    def _variable(self, do=True):
         state = 0
         var_value = None
         char = self.template.read(1)
@@ -94,7 +103,10 @@ class Template:
                     state = 3
                     self._ungetc(-1)
                     var_name = self._variable_name()
-                    var_value = self.context.get(var_name)
+                    if do:
+                        var_value = self.context.get(var_name)
+                    else:
+                        var_value = ''
                 elif char <> ' ':
                     raise SyntaxError('only blank can be \
                     between {{ and var', self._whoami())
@@ -115,7 +127,7 @@ class Template:
 
         return str(var_value)
         
-    def _tag(self):
+    def _tag(self, do=True):
         tag_result = None
         state = 0
         char = self.template.read(1)
@@ -138,7 +150,7 @@ class Template:
                     tag_name = self._get_tag_name()
                     if self.tags.has_key(tag_name):
                         self.tags_stack.append(tag_name)
-                        tag_result = self.tags[tag_name]()
+                        tag_result = self.tags[tag_name](do)
                         self.tags_stack.pop()
                         break
                     else:                        
@@ -165,7 +177,7 @@ class Template:
             char = self.template.read(1)
         return tag_name
 
-    def _loop(self):
+    def _loop(self, do=True):
         for_var_name = None
         in_var_name = None
         state = 0
@@ -218,23 +230,30 @@ class Template:
         if not last_state:
             #TODO other error need
             raise SyntaxError('not last state. It is end of file maybe', self._whoami())
-        
-        in_var_value = self.context.get(in_var_name)
-        if type(in_var_value) != type([]):
-            #TODO other error need
-            raise SyntaxError('type of var after in does not support', self._whoami())
-        
-        result = ''
-        start_loop = self.template.tell()
-        for for_var_value in in_var_value:
-            self.template.seek(start_loop)
-            self.context.set(for_var_name, for_var_value)
-            result += self._body()
-            
-        self.context.del_var(for_var_name)
+
+
+        result = ''        
+        if not do:
+            self._body(False)
+        else:
+            in_var_value = self.context.get(in_var_name)
+            if type(in_var_value) != type([]):
+                #TODO other error need
+                raise SyntaxError('type of var after in does not support', self._whoami())
+
+            start_loop = self.template.tell()
+            for for_var_value in in_var_value:
+                self.template.seek(start_loop)
+                self.context.set(for_var_name, for_var_value)
+                result += self._body()
+            if len(in_var_value) <> 0:
+                self.context.del_var(for_var_name)
+            else:
+                self._body(False)
+
         return result
 
-    def _condition(self):
+    def _condition(self, do=True):
         state = 0
         predicat_name = None
         last_state = False
@@ -268,20 +287,60 @@ class Template:
             raise SyntaxError('state is not last', self._whoami(), 
                               daddy = self._whosdaddy())
 
-        predicat = self.context.get(predicat_name)
-        if not type(predicat) is bool:
-            #TODO other error
-            raise SyntaxError('only bool is support', self._whoami())
-        
         result = ''
-        if predicat:
-            result += self._body()
+        if do:
+            predicat = self.context.get(predicat_name)
+            if not type(predicat) is bool:
+                #TODO other error
+                raise SyntaxError('only bool is support', self._whoami())
+
+            if predicat:
+                result += self._body()
+            else:
+                self._body(False)
         else:
-            self._goto_endtag()
+            self._body(False)
 
         return result
 
-    def _body(self):
+    def _block(self, do=False):
+        last_state = False
+        block_name = ''
+        state = 0
+        char = self.template.read(1)
+        while (len(char) == 1):
+            if state == 0:
+                if char.isalpha() or char == '_':
+                    self._ungetc(-1)
+                    block_name = self._variable_name()
+                    state = 1
+                elif char <> ' ':
+                    raise SyntaxError('expected blank', self._whoami())
+
+            elif state == 1:
+                if char == '%':
+                    state = 2
+                elif char <> ' ':
+                    raise SyntaxError('expected %', self._whoami())
+
+            elif state == 2:
+                if char == '}':
+                    last_state = True
+                    break
+                else:
+                    raise SyntaxError('expected }', self._whoami())
+            char = self.template.read(1)
+
+        if not last_state:
+            raise SyntaxError('state is not last')
+
+        if self.context.has_key(block_name):
+            self._body(False)
+            return self.context.get(block_name)
+        else:
+            return self._body()
+
+    def _body(self, do=True):
         result = ''
         state = 0
         buf = None
@@ -297,7 +356,7 @@ class Template:
             elif state == 1:
                 if char == '{':
                     self._ungetc(-2)
-                    result += self._variable()
+                    result += self._variable(do)
                     state = 0
                 elif char == '%':
                     self._ungetc(-2)
@@ -305,7 +364,7 @@ class Template:
                         self._endtag()
                         break
                     else:
-                        result += self._tag()
+                        result += self._tag(do)
                         state = 0
                 else:
                     result += buf + char
@@ -314,34 +373,6 @@ class Template:
         
         #TODO last state
         return result
-
-    def _goto_endtag(self):
-        last_state = False
-        state = 0
-        char = self.template.read(1)
-        while (len(char) == 1):
-            if state == 0:
-                if char == '{':
-                    state = 1
-            
-            elif state == 1:
-                if char == '{':
-                    self._ungetc(-2)
-                    self._variable()
-                    state = 0
-                elif char == '%':
-                    self._ungetc(-2)
-                    if self._is_endtag():
-                        self._endtag()
-                        last_state = True
-                        break
-                    else:
-                        self._tag()
-                        state = 0
-            char = self.template.read(1)
-
-        if not last_state:
-            raise SyntaxError('state is not last', self._whoami())
 
     def _is_endtag(self):
         ok = False
@@ -434,9 +465,6 @@ class Template:
         last_tag = self.tags_stack[-1]
         if self.pairs[last_tag] <> endtag_name:
             raise SyntaxError('end tag is not match', self._whoami())
-
-    
-
         
     def _get_endtag_name(self):
         endtag_name = ''
@@ -462,7 +490,150 @@ class Template:
                 break
             char = self.template.read(1)
         return var_name
+    
+    def _is_extends(self):
+        is_extends = False
+        state = 0
+        char = self.template.read(1)
+        while (len(char) == 1):
+            if state == 0:
+                if char == '{':
+                    state = 1
+                elif not char.isspace():
+                    break
+
+            elif state == 1:
+                if char == '%':
+                    state = 2
+                else:
+                    break
+
+            elif state == 2:
+                if char.isalpha():
+                    self._ungetc(-1)
+                    extends_str = self._get_extends_str()
+                    if extends_str == 'extends':
+                        state = 3
+                    else:
+                        break
+                elif char <> ' ':
+                    break
+
+            elif state == 3:
+                if char.isalpha() or char == '_':
+                    self._ungetc(-1)
+                    file_name = self._get_extends_file_name();
+                    state = 4
+                elif char <> ' ':
+                    break
+                
+            elif state == 4:
+                if char == '%':
+                    state = 5
+                elif char <> ' ':
+                    break
+
+            elif state == 5:
+                if char == '}':
+                    is_extends = True
+                else:
+                    break
+            char = self.template.read(1)
+
+        if is_extends:
+            self.extends_file_name = file_name
+        else:
+            self.template.seek(0)
+
+        return is_extends
+
+    def _extends(self):
+        blocks = self._get_blocks()
+        template = Template(self.extends_file_name)
+
+        dic_context = self.context.get_dict()
+        for key in blocks.keys():
+            dic_context[key] = blocks[key]
+        context = Context(dic_context)
+
+        return template.render(context)
+
+    def _get_blocks(self):
+        blocks = {}
+        state = 0
+        char = self.template.read(1)
+        while (len(char) == 1):
+            if state == 0:
+                if char == '{':
+                    state = 1
+            
+            elif state == 1:
+                if char == '%':
+                    state = 2
+            
+            elif state == 2:
+                if char.isalpha():
+                    self._ungetc(-1)
+                    tag_name = self._get_tag_name()
+                    if tag_name == 'block':
+                        position = self.template.tell()
+                        block_name = self._variable_name()
+                        self.template.seek(position)
+                        self.tags_stack.append(tag_name)
+                        blocks[block_name] = self.tags[tag_name]()
+                        state = 0
+                    else:
+                        raise SyntaxError('only block tag can be in file which extend', self._whoami())
+                    
+            char = self.template.read(1)
+
+        return blocks
+
+    def _get_extends_str(self):
+        state = 0
+        extends_str = ''
+        len_extends_str = len('extends')
+        num = 0
+        char = self.template.read(1)
+        while (len(char) == 1 and num < len_extends_str):
+            extends_str += char
+            num += 1
+            char = self.template.read(1)
+
+        if num == len_extends_str:
+            self._ungetc(-1)
+
+        return extends_str
+
+    def _get_extends_file_name(self):
+        last_state = False
+        state = 0
+        file_name = ''
+        char = self.template.read(1)
+        while (len(char) == 1):
+            if state == 0:
+                if char.isalpha() or char == '_':
+                    state = 1
+                    file_name += char
+                else:
+                    raise SyntaxError('error in file name which extends', self._whoami())
+
+            elif state == 1:
+                if char.isalnum() or char == '_' or char == '.':
+                    file_name += char
+                elif char == ' ':
+                    self._ungetc(-1)
+                    last_state = True
+                    break
+                else:
+                    raise SyntaxError('expected blank', self._whoami())
+
+            char = self.template.read(1)
+
+        if not last_state:
+            raise SyntaxError('state is not last', self._whoami())
         
+        return file_name
 
     def _ungetc(self, numb):
         self.template.seek(numb, os.SEEK_CUR)
@@ -481,6 +652,9 @@ class Context:
 
     def set(self, key, value):
         self.context[key] = value
+    
+    def has_key(self, key):
+        return self.context.has_key(key)
 
     def del_var(self, key):
         try:
@@ -489,6 +663,9 @@ class Context:
             #TODO exception
             pass
 
+    def get_dict(self):
+        return self.context
+
 class SyntaxError(Exception):
     def __init__(self, info='unknown', func_name=None, 
                  state=None, daddy=None):
@@ -496,6 +673,7 @@ class SyntaxError(Exception):
         self.state = None
         self.func_name = func_name
         self.daddy = daddy
+
     def __str__(self):
         text = 'SyntaxError: %s' % (self.info,)
         if self.state:
