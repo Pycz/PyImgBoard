d1b67979493d2deae92e370a05d233b7cc4fe36e
Marsel Zaripov <marszaripov@gmail.com>
2012-11-06 06:04:38 +0400

    change design

--- controllers/first.py
+++ controllers/first.py
@@ -3,13 +3,7 @@ from lib.template import Template, Context
 
 def index(request):
     template = Template('index.html')
-    a = [1, 2, 3, 4, 5]
-    b = [4, 8, 15, 16, 23, 42]
-    context = Context({'title': 'Главная страница', 
-                       'welcome_text': 'Добро пожаловать!', 
-                       'array1': a,
-                       'array2': b, 
-                       'pred': False})
+    context = Context({'welcome_text': 'Добро пожаловать'})
     result = template.render(context)
     return result
 
