2208937e116705c2eeacebd3532b803ffe0ea7e9
Borovilov Ruslan <pycz2007@yandex.ru>
2012-11-24 21:07:08 +0400

    Try to catch a bug

--- models.py
+++ models.py
@@ -4,13 +4,16 @@ import time
 from lib.utils import get_root_dir
 import sys
 
+def now_timestamp():
+    return sqlite3.TimestampFromTicks(time.time())
+
+
 class Record:
     '''Implements all records on board'''
 
     def __init__(self, 
                  T_id,
-                 date,
-                 time,
+                 timestamp,
                  name,
                  email,
                  title,
@@ -18,8 +21,7 @@ class Record:
                  image,
                  tread_id):
         self.id = T_id
-        self.date = date
-        self.time = time
+        self.timestamp = timestamp
         self.name = name
         self.email = email
         self.title = title
@@ -84,7 +86,7 @@ class Model:
         # update time of last adding
         self.conection.execute("""
         UPDATE %s SET last_time = :timestamp WHERE id = :id """ % (its_treads,),
-        {"timestamp": sqlite3.TimestampFromTicks(time.time()), 
+        {"timestamp": now_timestamp(), 
          "id": tread.id})
         
         self.conection.commit()
@@ -92,24 +94,41 @@ class Model:
     def get_all_records_from(self, tread, board = "B"):
         its_records = "records" + board
         #its_treads = "treads" + board
+        
         self.cur.execute(
         """
         SELECT * FROM (%s) WHERE tread_id = (:tread_id)
         """ % its_records, {"tread_id": str(tread.id)}
         )
+        
         return self._list_of_tuple_to_list_of_obj(self.cur.fetchall(), Record)
     
     def get_tread_by_id(self, T_id, board = "B"):
         its_treads = "treads" + board
+        
         self.cur.execute("""
         SELECT * FROM %s WHERE id = :tread_id""" % (its_treads,),
-        {"tread_id": T_id})
+        {"tread_id": T_id}
+        )
+        
         return self._list_of_tuple_to_list_of_obj(self.cur.fetchall(), Tread)
     
     def get_all_treads(self, board = "B"):
         its_treads = "treads" + board
+        
         self.cur.execute("""
-        SELECT * FROM %s """ % (its_treads,))
+        SELECT * FROM %s """ % (its_treads,)
+        )
+        
+        return self._list_of_tuple_to_list_of_obj(self.cur.fetchall(), Tread)
+    
+    def get_all_treads_by_date(self, board = "B"):
+        its_treads = "treads" + board
+        
+        self.cur.execute("""
+        SELECT * FROM %s ORDER BY timestamp""" % (its_treads,)
+        )
+        
         return self._list_of_tuple_to_list_of_obj(self.cur.fetchall(), Tread)
     
     
